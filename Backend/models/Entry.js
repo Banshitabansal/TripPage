0006245467cbe392bd881ac3d354d9882b03bd77
Banshitabansal <bbanshita7461@gmail.com>
2024-07-25 11:55:07 +0530

    25/July/2024

---
+++ Backend/models/Entry.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const entrySchema = new mongoose.Schema({
+  paymentId: String,
+  planID: String,
+  serialNo: String,
+  employeeId: String, 
+  employeeName: String, 
+  selectOption: String,
+  selectOptions: String,
+  selectSR: String,
+  currency: String,
+  mode: String,
+  amount: String,
+  remarks: String,
+  status: String,
+});
+
+const Entry = mongoose.model('Entry', entrySchema);
+
+export default Entry;
